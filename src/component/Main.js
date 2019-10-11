ab0d1cde7f6f00fa413b31f78e1366c009d46d35
harshit860 <harshitga@gmail.com>
2019-10-11 10:19:43 +0530

    bug fix

--- src/component/Main.js
+++ src/component/Main.js
@@ -134,7 +134,7 @@ class Main extends React.Component {
         return (
             <div>
                 <div style={{ width: "500px",  marginTop: "30px" }}>
-                    <input id={"input"} style={{ fontSize: "30px",  width: "500px", height: "50px" }} value={this.state.value}></input>
+                    <input id={"input"} style={{ fontSize: "30px", height: "50px" }} value={this.state.value}></input>
                     <h3>{this.state.operand1}</h3>
                     <h3>{this.state.operation}</h3>
                     <h3>{this.state.value}</h3>
@@ -146,30 +146,30 @@ class Main extends React.Component {
                         <button onClick={() => this.handleClick(1)}>1</button>
                         <button onClick={() => this.handleClick(2)}>2</button>
                         <button onClick={() => this.handleClick(3)}>3</button>
-                        <button onClick={() => this.handleClick("+")} style={{ width: "130px", fontSize: "50px", backgroundColor: "grey", color: "white" }}>+</button>
+                        <button onClick={() => this.handleClick("+")} style={{  fontSize: "50px", backgroundColor: "grey", color: "white" }}>+</button>
                     </div>
                     <div className="flex">
                         <button onClick={() => this.handleClick(4)}>4</button>
                         <button onClick={() => this.handleClick(5)}>5</button>
                         <button onClick={() => this.handleClick(6)}>6</button>
-                        <button onClick={() => this.handleClick("-")} style={{ width: "130px", fontSize: "50px", backgroundColor: "grey", color: "white" }}>-</button>
+                        <button onClick={() => this.handleClick("-")} style={{ fontSize: "50px", backgroundColor: "grey", color: "white" }}>-</button>
                     </div>
                     <div className="flex">
                         <button onClick={() => this.handleClick(7)}>7</button>
                         <button onClick={() => this.handleClick(8)}>8</button>
                         <button onClick={() => this.handleClick(9)}>9</button>
-                        <button onClick={() => this.handleClick("x")} style={{ width: "130px", fontSize: "50px", backgroundColor: "grey", color: "white" }}>x</button>
+                        <button onClick={() => this.handleClick("x")} style={{ fontSize: "50px", backgroundColor: "grey", color: "white" }}>x</button>
                     </div>
                     <div className="flex">
 
-                        <button onClick={() => this.handleClick("=")} style={{ width: "130px", fontSize: "50px", backgroundColor: "grey", color: "white" }}>=</button>
+                        <button onClick={() => this.handleClick("=")} style={{ fontSize: "50px", backgroundColor: "grey", color: "white" }}>=</button>
                         <button onClick={() => this.handleClick(0)}>0</button>
-                        <button onClick={() => this.handleClick("del")} style={{ width: "100px", fontSize: "30px", backgroundColor: "grey", color: "white" }}>DEL</button>
-                        <button onClick={() => this.handleClick("/")} style={{ width: "130px", fontSize: "50px", backgroundColor: "grey", color: "white" }}>/</button>
+                        <button onClick={() => this.handleClick("del")} style={{  fontSize: "30px", backgroundColor: "grey", color: "white" }}>DEL</button>
+                        <button onClick={() => this.handleClick("/")} style={{ fontSize: "50px", backgroundColor: "grey", color: "white" }}>/</button>
                     </div>
 
                     <div className="flex">
-                        <button id={"1"} style={{ width: "530px" }} onClick={() => this.flagset()}>History</button>
+                        <button id={"1"} style={{ width: "430px" }} onClick={() => this.flagset()}>History</button>
                     </div>
 
                     <div className="flex">
